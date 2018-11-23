ec7a9be330ba4e33b76a0a72a9d55b708899ef59
Ben Wexler <mixedywex@gmail.com>
2018-11-23 18:08:40 -0400

    about to start working on adding navbar icons

---
+++ app/assets/javascripts/latestTracks.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const fakeSubmitBtn = document.getElementById("fakeSubmitBtn");
+const createTrack = document.getElementById("createTrack");
+
+
+const soundcloudEmbed = document.getElementById("track_soundcloud_id")
+
+var spliceSoundcloudId = (event) => { 
+
+    if((event.type === "click") || (event.keyCode === 13)) {
+
+    soundcloudLink = soundcloudEmbed.value.split('tracks/')[1]
+    soundcloudEmbed.value = soundcloudLink.split('&')[0]
+    createTrack.submit()
+    }
+
+    
+    
+};
+
+
+fakeSubmitBtn.addEventListener("click", spliceSoundcloudId)
+
+fakeSubmitBtn.addEventListener("keydown", spliceSoundcloudId)
+
+
+});
