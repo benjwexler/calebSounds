478924b19dd3249d11c855194d682ecb7f25eefb
Ben Wexler <mixedywex@gmail.com>
2018-11-30 14:03:29 -0500

    forgot to migrate transactions table, also added data attribute of kit id to add to cart btn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_013011) do
+ActiveRecord::Schema.define(version: 2018_11_30_183249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,14 @@ ActiveRecord::Schema.define(version: 2018_11_30_013011) do
     t.string "name"
     t.integer "soundcloud_id"
     t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "kit_id"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
