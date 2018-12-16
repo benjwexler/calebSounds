273d3d548c0874590f6a2301b0925d50257a6de5
Ben Wexler <mixedywex@gmail.com>
2018-12-16 14:31:42 -0500

    going to have to display social media links in new place

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,3 +1,13 @@
-// document.getElementById("addToCartButton").addEventListener("click", function(){
-//     console.log("clicked")
-// });
+document.addEventListener("DOMContentLoaded", function () {
+
+document.getElementById("shoppingCartLink").addEventListener("mouseover", function(){
+    console.log("clicked")
+    document.querySelector(".App").style.display = "flex";
+});
+
+document.getElementById("shoppingCartLink").addEventListener("mouseout", function(){
+    console.log("clicked")
+    document.querySelector(".App").style.display = "none";
+});
+
+});
