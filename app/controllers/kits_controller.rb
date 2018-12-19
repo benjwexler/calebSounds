d601c32d73791ba370db8c187d642cbb12b86a41
Ben Wexler <mixedywex@gmail.com>
2018-12-18 20:57:08 -0500

    nested attributes are being created correctly now

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -89,7 +89,101 @@ class KitsController < ApplicationController
     p params 
     p soundfiles = params["kit"]["folder"]
     p  params["kit"]["name"]
-    @kit = Kit.new(:name => params["kit"]["name"], :description => params["kit"]["description"], :price => params["kit"]["price"], :quantity_sold => 0, :cover_art => params["kit"]["cover_art"])
+
+   
+    # @kit = Kit.new(:name => params["kit"]["name"], :description => params["kit"]["description"], :price => params["kit"]["price"], :quantity_sold => 0, :cover_art => params["kit"]["cover_art"])
+
+    sound_attributes_array = []
+    
+    
+
+#     params = { member: {
+#   name: 'joe', posts_attributes: [
+#     { title: 'Kari, the awesome Ruby documentation browser!' },
+#     { title: 'The egalitarian assumption of the modern citizen' },
+#     { title: '', _destroy: '1' } # this will be ignored
+#   ]
+# }}
+
+# member = Member.create(params[:member])
+    p @kit 
+
+    i = 1
+
+  
+
+    soundfiles.each do |soundfile|
+      # p soundfile
+
+     
+
+     soundfile_to_build = {
+       name: params["kit"]["name"], 
+      type_of_sound: params["kit"]["name"],
+      description: params["kit"]["name"],
+      key: params["kit"]["name"],
+      tempo: i,
+      soundfile: soundfile
+    }
+
+    # @new_sound = Sound.new(soundfile_to_build)
+
+    sound_attributes_array.push(soundfile_to_build)
+
+    # if @new_sound.save
+      
+    # end 
+
+        
+
+    # p @sound = Sound.new(:name => params["kit"]["name"], :type_of_sound => params["kit"]["name"] , :description => params["kit"]["name"], :key => params["kit"]["name"] , :tempo => i, :soundfile => soundfile)
+
+  
+
+    
+      # @sound.save
+
+    
+
+      
+      # i+=1
+      # if Sound.last
+      #   @last_sound = Sound.last.id
+      # else 
+      #   @last_sound = 1
+      # end 
+      #   @soundAndKit = SoundAndKit.new(:kit_id => @last_kit, :sound_id => @last_sound)
+      #   @soundAndKit.save 
+    end 
+
+    new_kit_params = {
+      kit: {
+        name: params["kit"]["name"], 
+        description: params["kit"]["description"], 
+        price: params["kit"]["price"], 
+        quantity_sold: 0,
+        cover_art: params["kit"]["cover_art"],
+        # sound_and_kits_attributes: [],
+        sounds_attributes: sound_attributes_array
+        # sounds_attributes: [{}]
+        
+      }
+    }
+    # p @kit = Kit.new()
+    # @kit.sounds.build
+
+    p sound_attributes_array
+    p "sssth"
+    p @kit = Kit.new(new_kit_params[:kit])
+
+    p@kit.sounds
+    
+
+    
+
+    # @kit.save
+
+   
 
     respond_to do |format|
       if @kit.save
@@ -101,26 +195,19 @@ class KitsController < ApplicationController
       end
     end
 
-    i = 1
-    if Kit.last
-      p @last_kit = Kit.last.id
-    else 
-      @last_kit = 1
-    end 
+   
+    # if Kit.last
+    #   p @last_kit = Kit.last.id
+    # else 
+    #   @last_kit = 1
+    # end 
 
-    soundfiles.each do |soundfile|
-      p soundfile
-      @sound = Sound.new(:name => params["kit"]["name"], :type_of_sound => params["kit"]["name"] , :description => params["kit"]["name"], :key => params["kit"]["name"] , :tempo => i, :soundfile => soundfile)
-      @sound.save
-      i+=1
-      if Sound.last
-        @last_sound = Sound.last.id
-      else 
-        @last_sound = 1
-      end 
-        @soundAndKit = SoundAndKit.new(:kit_id => @last_kit, :sound_id => @last_sound)
-        @soundAndKit.save 
-    end 
+    
+
+    
+
+   p "hizzah"
+
   end
 
   # PATCH/PUT /kits/1
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -3,12 +3,12 @@ class Sound < ApplicationRecord
     has_many :sound_and_kits
     has_many :kits, through: :sound_and_kits
 
-    def initialize(session)
-        @session = session
-      end
+    # def initialize(session)
+    #     @session = session
+    #   end
 
-      def add_item
-        # ... other item adding logic
-        @session[:temporary_cart][:num_items] += 1
-      end
+    #   def add_item
+    #     # ... other item adding logic
+    #     @session[:temporary_cart][:num_items] += 1
+    #   end
 end
