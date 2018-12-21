c6eeeed3d4466f1e7977db2f972b5e6d30e2c3bf
Ben Wexler <mixedywex@gmail.com>
2018-12-20 19:11:16 -0500

    sign in is working with devise

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  def new
+    redirect_to root_path
+    # redirect_to :root, id: 3, something: 'else'
+    # super
+
+   
+      # render 'redirect_user.js.erb'
+    
+    
+    p " hira"
+  end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
